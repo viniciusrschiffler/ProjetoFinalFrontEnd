fe81bfc7f910bb582a87f17d8431c60e37e20744
Natanalpe <natan.altomar14@gmail.com>
2021-10-27 23:48:35 -0300

    página de filmes
    Patch notes:
    
    1- Titulo da página agora está ligado ao nome do filme.
    
    2 - alterações mínimas para melhorar a Responsividade(inacabado).
    
    3 - Ícone 'Sucesso' corrigido no Pop-up de comprar/alugar.

--- src/pages/PaginaFilme/main.js
+++ src/pages/PaginaFilme/main.js
@@ -1,3 +1,13 @@
+const TOKEN = '345411636508e2b74308228fcfc87973'
+
+async function requisicoes(){
+    const capa = await fetch().json
+    const banner = await fetch().json
+    const nome = await fetch().json
+    const rating = await fetch().json
+    const sinopse = await fetch().json
+}
+
 var popUps = document.querySelectorAll('.botoes');
 
 popUps.forEach(popUp =>{
@@ -12,3 +22,7 @@ popUps.forEach(popUp =>{
         })
     })
 })
+
+function mudarTitulo(nomeFilme){
+    document.title = document.getElementById("tituloFilme").innerHTML;
+}
