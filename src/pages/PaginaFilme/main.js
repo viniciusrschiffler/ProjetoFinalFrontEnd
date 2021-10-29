b93c0c1477856525ae4d959bf310a203ef00f504
Natanalpe <natan.altomar14@gmail.com>
2021-10-29 16:41:31 -0300

    Página de filmes

--- src/pages/PaginaFilme/main.js
+++ src/pages/PaginaFilme/main.js
@@ -1,11 +1,45 @@
+/*==============================Requisição e uso da API==============================*/
 const TOKEN = '345411636508e2b74308228fcfc87973'
 
 async function requisicoes(){
-    const capa = await fetch().json
-    const banner = await fetch().json
-    const nome = await fetch().json
-    const rating = await fetch().json
-    const sinopse = await fetch().json
+    var filme = 541;
+    const dados = await (await fetch(`https://api.themoviedb.org/3/movie/${filme}?api_key=${TOKEN}`)).json();
+    var preco = dados.budget * 0.000001 + 2;
+    
+        document.getElementById("tituloFilme").innerHTML = dados.title;
+        document.getElementById("rating").innerHTML = dados.vote_average;
+        document.getElementById("sinopseTitulo").innerHTML = dados.overview;
+        document.querySelector("#capaFilme > img").setAttribute("src", `https://www.themoviedb.org/t/p/w220_and_h330_face${dados.poster_path}`)
+        document.querySelector("#background > img").setAttribute("src", `https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces${dados.backdrop_path}`)
+        document.getElementById("botaoComprarTexto").innerHTML = ("R$ " + preco);
+        
+        
+    }
+requisicoes();
+
+function ComprarFilme(){
+
+    var comprarButton = document.getElementById("#botaoComprarTexto")
+
+    function setFilme(){
+        comprarButton.addEventListener('click', ()=>{
+            localStorage.setItem('Nome', filme)
+            localStorage.setItem('Tipo', "COMPRAR")
+            localStorage.setItem('Preço', "R$" + preco)
+        })
+    }
+}
+function alugarFilme(){
+
+    var alugarButton = document.getElementById("#botaoAlugarTexto")
+
+    function setFilme(){
+        alugarButton.addEventListener('click', ()=>{
+            localStorage.setItem('Nome', filme)
+            localStorage.setItem('Tipo', "ALUGUEL")
+            localStorage.setItem('Preço', "R$" + preco)
+        })
+    }
 }
 
 var popUps = document.querySelectorAll('.botoes');
@@ -22,7 +56,6 @@ popUps.forEach(popUp =>{
         })
     })
 })
-
 function mudarTitulo(nomeFilme){
     document.title = document.getElementById("tituloFilme").innerHTML;
 }
